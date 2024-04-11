e9ea26233b818a1ce04f1a0727c787740cf40932
Sagnik <sagnik.sarkar22-24@bibs.co.in>
2024-04-11 18:58:11 +0530

    app created

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-#Metadata about the packagaes
 from setuptools import find_packages, setup
-from typing import List
+from typing import List 
 
-HYPHEN_E_DOT = "-e ."
+HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]: 
+def get_requirements(file_path:str)-> List[str]: 
     requirements = []
-    with open(file_path) as file_obj:
+    with open(file_path) as file_obj: 
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements = [req.replace ("\n", "") for req in requirements]
 
         if HYPHEN_E_DOT in requirements: 
             requirements.remove(HYPHEN_E_DOT)
     return requirements
 
 setup(
-    name = "LP_ML_Project", 
-    version= "0.0.1", 
+    name = "PipelineProject", 
+    version = "0.0.1", 
     author = "Sagnik Sarkar", 
-    description= "An End-to-end ML pipeline project about Diamond price prediction", 
-    author_email="sagnik.sarkar22-24@bibs.co.in", 
+    description= "A trial End to End ML project created for First batch of LP students",
+    author_email= "sagnik.sarkar22-24@bibs.co.in", 
     install_requires = get_requirements("requirements.txt"),
-    packages=find_packages()
-)
+    packages= find_packages()
+    )
